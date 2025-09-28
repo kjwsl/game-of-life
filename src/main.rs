13d18761cc941d9ae323a8ac8ee26189e14cdc55
Ray <kjwsl@fatherslab.com>
2025-09-29 01:40:32 +0900

    feat: add clap for parsing args

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
+use clap::Parser;
 use game_of_life::Canvas;
 
+const DEFAULT_INTERVAL: u64 = 300;
+
+#[derive(Parser)]
+struct Args {
+    canvas_path: String,
+
+    /// The number of milliseconds to wait between each tick
+    #[clap(short, long)]
+    interval: Option<u64>,
+}
+
 fn main() {
-    let ms = 300;
-    let canvas_path = std::env::args().nth(1).expect("Specify canvas path");
-    let canvas_file = std::fs::read_to_string(canvas_path).expect("Failed to read canvas path");
+    let args = Args::parse();
+
+    let ms = args.interval.unwrap_or(DEFAULT_INTERVAL);
+    let canvas_file = std::fs::read_to_string(args.canvas_path).expect("Failed to read canvas path");
 
     let mut canvas = Canvas::from_str(&canvas_file);
 
